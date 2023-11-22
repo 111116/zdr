94b04f6edf5e73a0796048e50357ffcb59d1686e
bilibili <zhou.zq@uci.edu>
2023-11-21 23:04:30 -0800

    add a bit of documentation

--- render.py
+++ render.py
@@ -1,15 +1,23 @@
+"""Simple Differentiable Renderer using Luisa.
+
+Currently only supports collocated direct lighting.
+
+"""
 import torch
 import luisa
 from luisa.mathtypes import *
 import numpy as np
 from PIL import Image
 import weakref
+import gc
+from time import time
 
 from load_obj import read_obj, concat_triangles
 from recompute_normal import recompute_normal
 from uvgrad import render_uvgrad_kernel
 from integrator import render_kernel, render_backward_kernel
 
+# Using CUDA for interaction with PyTorch
 luisa.init('cuda')
 
 
@@ -22,8 +30,31 @@ def float3list_to_padded_tensor(l):
     return w.as_strided(size=(n,3), stride=(4,1))
 
 class Scene:
+    """A class representing a 3D scene for differentiable rendering.
+
+    The Scene class is responsible for setting up the 3D environment, 
+    which includes loading the geometry from an OBJ file, setting up 
+    materials, and configuring the camera. It provides functionalities 
+    for forward and backward rendering using the differentiable rendering 
+    technique.
+
+    Attributes:
+        camera (struct): Camera settings including FOV, origin, target, and up vector.
+            fov (float): The field of view of the camera in radians
+            origin (float3): The position of the camera in the 3D world space.
+            target (float3): The point in the 3D space that the camera is looking at.
+            up (float3): The up vector of the camera, typically (0,1,0).
+
+        render_kernel (function): Kernel function for rendering.
+
+    Args:
+        obj_file (str): Path to the OBJ file containing the 3D model geometry.
+        use_face_normal (bool, optional): Flag to determine whether to use face normals. 
+                                          Will recompute vertex normals from faces if True.
+                                          Defaults to False.
+
+    """
     def __init__(self, obj_file, use_face_normal=False):
-        # load geometry from obj file
         # TODO recompute normal if not availble
         positions, tex_coords, normals, faces = read_obj(obj_file)
         self.v_buffer = luisa.Buffer.from_dlpack(float3list_to_padded_tensor(positions))
@@ -56,18 +87,25 @@ class Scene:
         luisa.synchronize()
         return image
     
-    def render_backward(self, grad_output, material, res, spp, seed):
+    def render_backward(self, grad_output, d_material, material, res, spp, seed):
         assert material.ndim == 3 and material.shape[2] == 4
         texture_res = material.shape[0:2]
-        material_buffer = luisa.Buffer.from_dlpack(material.detach().flatten())
-        # Can't use empty_like here because d_material have to be contiguous
-        d_material = torch.empty(material.size(), dtype=material.dtype, device=material.device)
+        material_buffer = luisa.Buffer.from_dlpack(material.flatten())
         d_material_buffer = luisa.Buffer.from_dlpack(d_material.flatten())
         d_image = luisa.Buffer.from_dlpack(grad_output.reshape((res[0]*res[1], 4)).contiguous())
+
+        # def namestr(obj, namespace):
+        #     return [name for name in namespace if namespace[name] is obj]
+        # referrers = gc.get_referrers(grad_output)
+        # for referrer in referrers:
+        #     print("- ", namestr(referrer, globals()), namestr(referrer, locals()))
+
+        # print("REF", len(gc.get_referrers(grad_output)), len(gc.get_referrers(d_image)))
         render_backward_kernel(d_image,
             self.v_buffer, self.vt_buffer, self.vn_buffer, self.triangle_buffer, self.accel,
             d_material_buffer, material_buffer, int2(*texture_res), self.camera,
             spp, seed+1, dispatch_size=res)
+        # print("REF", len(gc.get_referrers(grad_output)), len(gc.get_referrers(d_image)))
         luisa.synchronize()
         return d_material, None, None, None, None
 
@@ -77,26 +115,47 @@ class Scene:
             ctx.save_for_backward(material)
             ctx.scene = weakref.ref(self)
             ctx.args = args
-            return self.render_forward(material, *args)
+            return self.render_forward(material.detach(), *args)
         
         @staticmethod
         def backward(ctx, grad_output):
+            # print("a", len(gc.get_referrers(grad_output)))
             material, = ctx.saved_tensors
-            return ctx.scene().render_backward(grad_output, material, *ctx.args)
-            
+            if material.grad is None:
+                # Can't use empty_like here because d_material have to be contiguous
+                material.grad = torch.empty(material.size(), dtype=material.dtype, device=material.device)
+            return ctx.scene().render_backward(grad_output, material.grad, material.detach(), *ctx.args)
+
     def render(self, material, *, res, spp, seed):
+        """Renders the scene with the given material and rendering parameters.
+
+        This operation is differentiable w.r.t. material.
+
+        Args:
+            material (torch.Tensor): A tensor of shape (wu,hu,c) # TODO
+            res (tuple): Resolution of the rendered image as a tuple (width, height).
+            spp (int): Samples per pixel for rendering. Higher values increase quality at the
+                       cost of more computation.
+            seed (int): Seed for random number in rendering.
+
+        Returns:
+            torch.Tensor: A tensor (height, width, 4) representing the rendered image.
+
+        """
         return Scene.RenderOperator.apply(material, self, res, spp, seed)
 
 
-if __name__ == "__main__":
 
+# Example Usage
+if __name__ == "__main__":
+    from tqdm import tqdm
     def load_material(diffuse_file, roughness_file):
         from torchvision.transforms import ToTensor
         diffuse_img = ToTensor()(Image.open(diffuse_file)).cuda()
         roughness_img = ToTensor()(Image.open(roughness_file)).cuda()
         assert roughness_img.shape[0] == 1
         mat = torch.vstack((diffuse_img, roughness_img)).permute((1,2,0))**2.2
-        return mat
+        return mat.contiguous()
     obj_file = 'assets/sphere.obj'
     scene = Scene(obj_file, use_face_normal=True)
     diffuse_file = 'assets/wood_olive/wood_olive_wood_olive_basecolor.png'
@@ -104,8 +163,11 @@ if __name__ == "__main__":
     material = load_material(diffuse_file, roughness_file)
     material.requires_grad_()
     # scene.render_kernel = render_uvgrad_kernel
-    I = scene.render(material, res=(1024,1024), spp=1, seed=0)
+    for it in tqdm(range(1000)):
+        I = scene.render(material, res=(1024,1024), spp=1, seed=0)
+        I.sum().backward()
+
+    quit()
     Image.fromarray((I[...,0:3].clamp(min=0,max=1)**0.454*255).to(torch.uint8).cpu().numpy()).save('a.png')
-    I.sum().backward()
     Image.fromarray((material.grad[...,0:3].clamp(min=0, max=1)**0.454*255).cpu().numpy().astype("uint8")).save("d.png")
     Image.fromarray((material.grad[...,3].clamp(min=0, max=1)**0.454*255).cpu().numpy().astype("uint8")).save("dr.png")
