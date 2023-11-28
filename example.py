a57a976e0538cee07ad10ec2e9479164a1c6f07a
bilibili <zhou.zq@uci.edu>
2023-11-28 00:04:37 -0800

    implement bilinear texture interpolation

--- example.py
+++ example.py
@@ -22,27 +22,26 @@ scene = Scene(obj_file, use_face_normal=True)
 diffuse_file = 'assets/wood_olive/wood_olive_wood_olive_basecolor.png'
 roughness_file = 'assets/wood_olive/wood_olive_wood_olive_roughness.png'
 material_GT = load_material(diffuse_file, roughness_file)
-I_GT = scene.render(material_GT, res=(1024,1024), spp=64, seed=random.randint(0, 2147483647))
+I_GT = scene.render(material_GT, res=(1024,1024), spp=128) # seed defaults to 0
 Image.fromarray((I_GT[...,0:3].clamp(min=0,max=1)**0.454*255).to(torch.uint8).cpu().numpy()).save('results/gt.png')
-
-quit()
 
 # scene.render_kernel = render_uvgrad_kernel
 
 
 # ======== Optimization using differentiable rendering ========
 
-# Optimize using gradient descent
+# gradient descent
 material = torch.rand((1024,1024,4), device='cuda')
 material.requires_grad_()
 optimizer = torch.optim.Adam([material], lr=0.01)
 for it in tqdm(range(1000)):
     optimizer.zero_grad()
-    I = scene.render(material, res=(1024,1024), spp=4, seed=random.randint(0, 2147483647))
+    I = scene.render(material, res=(1024,1024), spp=1, seed=random.randint(0, 2147483647))
     ((I-I_GT)**2).sum().backward()
     optimizer.step()
-    material.data.clamp_(min=0, max=1)
+    material.data.clamp_(min=1e-3, max=1)
 
+I = scene.render(material, res=(1024,1024), spp=16)
 Image.fromarray((I[...,0:3].clamp(min=0,max=1)**0.454*255).to(torch.uint8).cpu().numpy()).save('results/a.png')
 Image.fromarray((material[...,0:3].clamp(min=0, max=1)**0.454*255).detach().cpu().numpy().astype("uint8")).save("results/d.png")
 Image.fromarray((material[...,3].clamp(min=0, max=1)**0.454*255).detach().cpu().numpy().astype("uint8")).save("results/dr.png")
--- integrator.py
+++ integrator.py
@@ -4,31 +4,59 @@ from luisa.autodiff import requires_grad, autodiff, backward, grad
 from .microfacet import ggx_brdf
 from .camera import generate_ray, tent_warp
 
+bilinear = True
+
 @luisa.func
-def get_uv_coord(uv: float2, texture_res: int2):
-    p = float2(uv.x, 1.0-uv.y) * float2(texture_res-1)
-    ip = int2(p)
-    off = p - float2(ip)
-    # TODO boundary check
-    nearest = int2(p+0.499)
-    return nearest.x + texture_res.x * nearest.y
+def read_single_bsdf(coord: int2, material_buffer, texture_res):
+    idx = coord.x + texture_res.x * coord.y
+    return float4(
+        material_buffer.read(idx * 4 + 0),
+        material_buffer.read(idx * 4 + 1),
+        material_buffer.read(idx * 4 + 2),
+        material_buffer.read(idx * 4 + 3))
 
 @luisa.func
 def read_bsdf(uv: float2, material_buffer, texture_res):
-    coord = get_uv_coord(uv, texture_res)
-    return float4(
-        material_buffer.read(coord * 4 + 0),
-        material_buffer.read(coord * 4 + 1),
-        material_buffer.read(coord * 4 + 2),
-        material_buffer.read(coord * 4 + 3))
+    if not bilinear:
+        p = float2(uv.x, 1.0-uv.y) * float2(texture_res-1)
+        nearest = int2(p+0.4999)
+        return read_single_bsdf(nearest, material_buffer, texture_res)
+    else:
+        p = float2(uv.x, 1.0-uv.y) * float2(texture_res-1)
+        ip = int2(p)
+        off = p - float2(ip)
+        c00 = read_single_bsdf(ip + int2(0,0), material_buffer, texture_res)
+        c01 = read_single_bsdf(ip + int2(0,1), material_buffer, texture_res)
+        c10 = read_single_bsdf(ip + int2(1,0), material_buffer, texture_res)
+        c11 = read_single_bsdf(ip + int2(1,1), material_buffer, texture_res)
+        return lerp(lerp(c00, c01, off.y), lerp(c10, c11, off.y), off.x)
+
+@luisa.func
+def write_single_bsdf_grad(coord: int2, dmat, d_material_buffer, texture_res):
+    idx = coord.x + texture_res.x * coord.y
+    _ = d_material_buffer.atomic_fetch_add(idx * 4 + 0, dmat.x)
+    _ = d_material_buffer.atomic_fetch_add(idx * 4 + 1, dmat.y)
+    _ = d_material_buffer.atomic_fetch_add(idx * 4 + 2, dmat.z)
+    _ = d_material_buffer.atomic_fetch_add(idx * 4 + 3, dmat.w)
 
 @luisa.func
 def write_bsdf_grad(uv: float2, dmat, d_material_buffer, texture_res):
-    coord = get_uv_coord(uv, texture_res)
-    _ = d_material_buffer.atomic_fetch_add(coord * 4 + 0, dmat.x)
-    _ = d_material_buffer.atomic_fetch_add(coord * 4 + 1, dmat.y)
-    _ = d_material_buffer.atomic_fetch_add(coord * 4 + 2, dmat.z)
-    _ = d_material_buffer.atomic_fetch_add(coord * 4 + 3, dmat.w)
+    if not bilinear:
+        p = float2(uv.x, 1.0-uv.y) * float2(texture_res-1)
+        nearest = int2(p+0.4999)
+        write_single_bsdf_grad(nearest, dmat, d_material_buffer, texture_res)
+    else:
+        p = float2(uv.x, 1.0-uv.y) * float2(texture_res-1)
+        ip = int2(p)
+        off = p - float2(ip)
+        k00 = (1-off.x) * (1-off.y)
+        k01 = (1-off.x) * off.y
+        k10 = off.x * (1-off.y)
+        k11 = off.x * off.y
+        write_single_bsdf_grad(ip + int2(0,0), k00 * dmat, d_material_buffer, texture_res)
+        write_single_bsdf_grad(ip + int2(0,1), k01 * dmat, d_material_buffer, texture_res)
+        write_single_bsdf_grad(ip + int2(1,1), k11 * dmat, d_material_buffer, texture_res)
+        write_single_bsdf_grad(ip + int2(1,1), k11 * dmat, d_material_buffer, texture_res)
 
 @luisa.func
 def direct_collocated(ray, v_buffer, vt_buffer, vn_buffer, triangle_buffer,
