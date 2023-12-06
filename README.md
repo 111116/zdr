175613c67c944b70d89bc09e492481638c5a60ad
bilibili <zhou.zq@uci.edu>
2023-12-06 12:00:35 -0800

    update document

--- README.md
+++ README.md
@@ -2,9 +2,7 @@
 
 Simple raytracing differentiable renderer (w.r.t. material parameters). Images and parameters are both PyTorch tensors.
 
-Currently only supports collocated direct lighting (point light at position of the camera).
-
-work in progress
+Currently only supports single object, collocated direct lighting (point light at position of the camera).
 
 ## Dependency
 
--- example.py
+++ example.py
@@ -1,6 +1,5 @@
 import torch
 import random
-from matplotlib import pyplot as plt
 from PIL import Image
 from tqdm import tqdm
 import imageio
@@ -22,38 +21,38 @@ scene = Scene(obj_file, use_face_normal=True)
 diffuse_file = 'assets/wood_olive/wood_olive_wood_olive_basecolor.png'
 roughness_file = 'assets/wood_olive/wood_olive_wood_olive_roughness.png'
 material_GT = load_material(diffuse_file, roughness_file)
-I_GT = scene.render(material_GT, res=(1024,1024), spp=128) # seed defaults to 0
+ImgRes = 1024, 1024
+print("Image resolution:", ImgRes)
+I_GT = scene.render(material_GT, res=ImgRes, spp=128) # seed defaults to 0
 Image.fromarray((I_GT[...,0:3].clamp(min=0,max=1)**0.454*255).to(torch.uint8).cpu().numpy()).save('results/gt.png')
-duvdxy = scene.render_duvdxy(material_GT, res=(1024,1024), spp=128) # seed defaults to 0
-Image.fromarray(((duvdxy[...,0:3]*1000+0.5).clamp(min=0,max=1)**0.454*255).to(torch.uint8).cpu().numpy()).save('results/duvdx_dudy.png')
-pixel_independency = torch.det(duvdxy.reshape(1024,1024,2,2)*1024).abs()
-Image.fromarray((pixel_independency.clamp(min=0,max=1)**0.454*255).to(torch.uint8).cpu().numpy()).save('results/pixel_independency.png')
-pixel_independency1 = sum(torch.det(scene.render_duvdxy(material_GT,
-                                                        res=(1024,1024),
-                                                        spp=1,
-                                                        seed=random.randint(0, 2147483647)
-                                                        ).reshape(1024,1024,2,2)*1024).abs() for _ in range(128))/128
-Image.fromarray((pixel_independency1.clamp(min=0,max=1)**0.454*255).to(torch.uint8).cpu().numpy()).save('results/pixel_independency1.png')
 
-# scene.render_kernel = render_uvgrad_kernel
+# duv/dxy (screen space to texture space jacobian)
+duvdxy = scene.render_duvdxy(material_GT, res=ImgRes, spp=128) # seed defaults to 0
+Image.fromarray(((duvdxy[...,0:3]*1000+0.5).clamp(min=0,max=1)**0.454*255).to(torch.uint8).cpu().numpy()).save('results/duvdx_dudy.png')
 
 
 # ======== Optimization using differentiable rendering ========
+# Note that this is just an example, where scene.camera remains unchanged.
 
 # gradient descent
-material = torch.rand((1024,1024,4), device='cuda')
+TexRes = 1024, 1024
+print("Texture resolution:", TexRes)
+material = torch.rand((*TexRes,4), device='cuda')
 material.requires_grad_()
 optimizer = torch.optim.Adam([material], lr=0.01)
-for it in tqdm(range(1000)):
-    optimizer.zero_grad()
-    I = scene.render(material, res=(1024,1024), spp=4, seed=random.randint(0, 2147483647))
+for it in tqdm(range(500)):
+    I = scene.render(material, res=ImgRes, spp=4, seed=random.randint(0, 2147483647))
     ((I-I_GT)**2).sum().backward()
     optimizer.step()
+    optimizer.zero_grad()
     material.data.clamp_(min=1e-3, max=1)
 
-I = scene.render(material, res=(1024,1024), spp=16)
+# Rendered image of reconstruction
+I = scene.render(material, res=ImgRes, spp=16)
+print("MSE", ((I-I_GT)**2).mean().item())
 Image.fromarray((I[...,0:3].clamp(min=0,max=1)**0.454*255).to(torch.uint8).cpu().numpy()).save('results/a.png')
+
+# Reconstructed texture
 Image.fromarray((material[...,0:3].clamp(min=0, max=1)**0.454*255).detach().cpu().numpy().astype("uint8")).save("results/d.png")
 Image.fromarray((material[...,3].clamp(min=0, max=1)**0.454*255).detach().cpu().numpy().astype("uint8")).save("results/dr.png")
-print("loss", ((I-I_GT)**2).sum().item())
 
