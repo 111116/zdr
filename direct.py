8cabddbf46c05f47db0372ff8d25ca6546e58ef3
bilibili <zhou.zq@uci.edu>
2023-12-13 11:55:22 -0800

    add onb (makes bsdf compute in local coords)

--- direct.py
+++ direct.py
@@ -5,6 +5,7 @@ from .microfacet import ggx_brdf
 from .integrator import derive_render_kernel, derive_render_backward_kernel
 from .interaction import read_bsdf, write_bsdf_grad, surface_interact
 from .light import sample_light
+from .onb import *
 
 @luisa.func
 def direct_estimator(ray, sampler, heap, accel, light_count, material_buffer, texture_res):
@@ -24,13 +25,14 @@ def direct_estimator(ray, sampler, heap, accel, light_count, material_buffer, te
     shadow_ray = luisa.make_ray(it.p, light.wi, 1e-4, light.dist)
     occluded = accel.trace_any(shadow_ray, -1)
     cos_wi_light = dot(light.wi, it.ns)
+    onb = make_onb(it.ns)
     # there could still be light from back face due to imperfect occlusion
     if not occluded and cos_wi_light > 0.0:
         mat = read_bsdf(it.uv, material_buffer, texture_res)
         diffuse = mat.xyz
         roughness = mat.w
         specular = 0.04
-        bsdf = ggx_brdf(-ray.get_dir(), light.wi, it.ns, diffuse, specular, roughness)
+        bsdf = ggx_brdf(onb.to_local(-ray.get_dir()), onb.to_local(light.wi), diffuse, specular, roughness)
         # mis_weight = balanced_heuristic(light.pdf, pdf_bsdf)
         mis_weight = 1.0
         radiance += bsdf * cos_wi_light * mis_weight * light.eval / max(light.pdf, 1e-4)
@@ -51,6 +53,7 @@ def direct_estimator_backward(ray, sampler, heap, accel, light_count,
     shadow_ray = luisa.make_ray(it.p, light.wi, 1e-4, light.dist)
     occluded = accel.trace_any(shadow_ray, -1)
     cos_wi_light = dot(light.wi, it.ns)
+    onb = make_onb(it.ns)
     if not occluded:
         mat = read_bsdf(it.uv, material_buffer, texture_res)
         with autodiff():
@@ -58,7 +61,7 @@ def direct_estimator_backward(ray, sampler, heap, accel, light_count,
             diffuse = mat.xyz
             roughness = mat.w
             specular = 0.04
-            bsdf = ggx_brdf(-ray.get_dir(), light.wi, it.ns, diffuse, specular, roughness)
+            bsdf = ggx_brdf(onb.to_local(-ray.get_dir()), onb.to_local(light.wi), diffuse, specular, roughness)
             # mis_weight = balanced_heuristic(light.pdf, pdf_bsdf)
             mis_weight = 1.0
             le = bsdf * cos_wi_light * mis_weight * light.eval / max(light.pdf, 1e-4)
--- microfacet.py
+++ microfacet.py
@@ -1,10 +1,13 @@
 import luisa
+from luisa import float2, float3
 from math import pi
 
+
 @luisa.func
-def ggx_distribution(n, h, alpha):
+def ggx_distribution(h, alpha):
+    # isotropic Trowbridge-Reitz distribution
     alpha2 = alpha * alpha
-    nh = max(0.00001, dot(n,h))
+    nh = max(0.00001, h.z)
     return alpha2 / (pi * ((nh * nh * (alpha2 - 1) + 1)) ** 2)
 
 @luisa.func
@@ -12,16 +15,17 @@ def fresnel_schlick(cos_theta, specular):
     return specular + (1 - specular) * (1 - cos_theta) ** 5
 
 @luisa.func
-def smith_geometry(n, v, alpha):
-    nv = max(0.00001, dot(n,v))
+def smith_geometry(v, alpha):
     alpha2 = alpha * alpha
+    nv = max(0.00001, v.z)
     return 2 / (1 + sqrt(1 + alpha2 * (1 - nv * nv) / (nv * nv)))
 
 @luisa.func
-def ggx_brdf(wi, wo, n, diffuse, specular, roughness):
+def ggx_brdf(wi, wo, diffuse, specular, roughness):
     alpha = roughness ** 2
     h = normalize(wi + wo)
-    d = ggx_distribution(n, h, alpha)
+    d = ggx_distribution(h, alpha)
     f = fresnel_schlick(clamp(dot(wo,h), 0.00001, 1.0), specular)
-    g = smith_geometry(n, wi, alpha) * smith_geometry(n, wo, alpha)
-    return ((d * f * g) / (4 * max(0.00001, dot(n,wi)) * max(0.00001, dot(n,wo))) + diffuse/pi) * dot(wo,n)
+    g = smith_geometry(wi, alpha) * smith_geometry(wo, alpha)
+    return ((d * f * g) / (4 * max(0.00001, wi.z) * max(0.00001, wo.z)) + diffuse/pi) * wo.z
+
