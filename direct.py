10fd5727bbfdb21ace3a2e597f26a7b988729cce
bilibili <zhou.zq@uci.edu>
2023-12-14 13:52:56 -0800

    tested cosine hemisphere bsdf sampling

--- direct.py
+++ direct.py
@@ -1,7 +1,7 @@
 import luisa
 from luisa.mathtypes import *
 from luisa.autodiff import requires_grad, autodiff, backward, grad
-from .microfacet import ggx_brdf
+from .microfacet import ggx_brdf, ggx_sample, ggx_sample_pdf
 from .integrator import derive_render_kernel, derive_render_backward_kernel
 from .interaction import read_bsdf, write_bsdf_grad, surface_interact
 from .light import sample_light
@@ -20,6 +20,13 @@ def direct_estimator(ray, sampler, heap, accel, light_count, material_buffer, te
     if any(emission > float3(0.0)):
         return emission
 
+
+    mat = read_bsdf(it.uv, material_buffer, texture_res)
+    diffuse = mat.xyz
+    roughness = mat.w
+    specular = 0.04
+
+    # direct light sample
     radiance = float3(0.0)
     light = sample_light(it.p, light_count, heap, sampler) # (wi, dist, pdf, eval)
     shadow_ray = luisa.make_ray(it.p, light.wi, 1e-4, light.dist)
@@ -29,14 +36,31 @@ def direct_estimator(ray, sampler, heap, accel, light_count, material_buffer, te
     wi_light_local = onb.to_local(light.wi)
     # there could still be light from back face due to imperfect occlusion
     if not occluded and wi_light_local.z > 0.0:
-        mat = read_bsdf(it.uv, material_buffer, texture_res)
-        diffuse = mat.xyz
-        roughness = mat.w
-        specular = 0.04
         bsdf = ggx_brdf(wo_local, wi_light_local, diffuse, specular, roughness)
         # mis_weight = balanced_heuristic(light.pdf, pdf_bsdf)
-        mis_weight = 1.0
+        mis_weight = 0.0
         radiance += bsdf * mis_weight * light.eval / max(light.pdf, 1e-4)
+
+    # pdf sample (next bounce)
+    wi_local = ggx_sample(wo_local, diffuse, specular, roughness, sampler)
+    pdf_bsdf = ggx_sample_pdf(wo_local, wi_local, diffuse, specular, roughness)
+    wi = onb.to_world(wi_local)
+    if dot(wi, it.ng) < 1e-4:
+        return radiance
+    ray = luisa.make_ray(luisa.offset_ray_origin(it.p, it.ng), wi, 0.0, 1e30)
+    beta = ggx_brdf(wo_local, wi_local, diffuse, specular, roughness) / pdf_bsdf
+    # JUST REPEAT..
+    hit = accel.trace_closest(ray, -1)
+    if hit.miss():
+        return radiance
+    it = surface_interact(hit, heap)
+    # backfacing geometry
+    if dot(-ray.get_dir(), it.ng) < 1e-4 or dot(-ray.get_dir(), it.ns) < 1e-4:
+        return radiance
+    emission = heap.buffer_read(float3, 23333, hit.inst)
+    if any(emission > float3(0.0)):
+        return radiance + beta * emission
+
     return radiance
 
 
--- microfacet.py
+++ microfacet.py
@@ -21,11 +21,81 @@ def smith_geometry(v, alpha):
     return 2 / (1 + sqrt(1 + alpha2 * (1 - nv * nv) / (nv * nv)))
 
 @luisa.func
-def ggx_brdf(wi, wo, diffuse, specular, roughness):
+def ggx_brdf(wo, wi, diffuse, specular, roughness):
     alpha = roughness ** 2
     h = normalize(wi + wo)
     d = ggx_distribution(h, alpha)
     f = fresnel_schlick(clamp(dot(wo,h), 0.00001, 1.0), specular)
     g = smith_geometry(wi, alpha) * smith_geometry(wo, alpha)
-    return ((d * f * g) / (4 * max(0.00001, wi.z) * max(0.00001, wo.z)) + diffuse/pi) * wo.z
+    return ((d * f * g) / (4 * max(0.00001, wi.z) * max(0.00001, wo.z)) + diffuse/pi) * wi.z
+
+
+@luisa.func
+def cosine_sample_hemisphere(u: float2):
+    r = sqrt(u.x)
+    phi = 2 * pi * u.y
+    return float3(r * cos(phi), r * sin(phi), sqrt(1.0 - u.x))
+
+@luisa.func
+def reflect(wo, n):
+    return -wo + 2 * dot(wo, n) * n
+
+@luisa.func
+def ggx_sample(wo, diffuse, specular, roughness, sampler):
+    alpha = roughness ** 2
+    u = sampler.next2f()
+    # sample diffuse
+    diffuse_sample_wi = cosine_sample_hemisphere(u)
+    diffuse_sample_pdf = diffuse_sample_wi.z / pi
+    # sample gloss
+    wm = sample_wm(wo, alpha, u)
+    glossy_sample_wi = reflect(wo, wm)
+    glossy_sample_pdf = pdf_wm(wo, wm, alpha) / (4 * abs(dot(wo, wm)))
+    # importance sampling
+    # w1 = ggx_brdf(diffuse_sample_wi, wo, diffuse, specular, roughness) / diffuse_sample_pdf
+    # w2 = ggx_brdf(glossy_sample_wi, wo, diffuse, specular, roughness) / glossy_sample_pdf
+    # TODO only sampling glossy lobe now
+    return diffuse_sample_wi
+
+@luisa.func
+def ggx_sample_pdf(wo, wi, diffuse, specular, roughness):
+    return wi.z / pi
+    alpha = roughness ** 2
+    wm = normalize(wi + wo)
+    # TODO only sampling glossy lobe now
+    return pdf_wm(wo, wm, alpha) / (4 * abs(dot(wo, wm)))
+
+
+@luisa.func
+def SampleUniformDiskPolar(u: float2):
+    r = sqrt(u.x)
+    theta = 2 * pi * u.y
+    return float2(r * cos(theta), r * sin(theta))
+
+@luisa.func
+def pdf_wm(w: float3, wm: float3, alpha: float):
+    return ggx_distribution(wm, alpha) * smith_geometry(w, alpha) * abs(w.z) * abs(dot(w, wm))
+
+@luisa.func
+def sample_wm(w: float3, alpha: float, u: float2) -> float3:
+    """sample visible normal from Trowbridge-Reitz distribution (pbrt-v4)
+    """
+    # Transform _w_ to hemispherical configuration
+    wh = normalize(float3(alpha * w.xy, w.z))
+    if wh.z < 0:
+        wh = -wh
+    # Find orthonormal basis for visible normal sampling
+    T1 = normalize(cross(float3(0,0,1), wh)) if wh.z < 0.9999 else float3(1,0,0)
+    T2 = cross(wh, T1)
+    # Generate uniformly distributed points on the unit disk
+    p = SampleUniformDiskPolar(u)
+    # Warp hemispherical projection for visible normal sampling
+    h = sqrt(1 - p.x**2)
+    p.y = lerp((1+wh.z)/2, h, p.y)
+    # Reproject to hemisphere and transform normal to ellipsoid configuration
+    pz = sqrt(max(0.0, 1.0 - length_squared(p)))
+    nh = p.x * T1 + p.y * T2 + pz * wh
+    # nh.z==0 should be rare
+    return normalize(float3(alpha * nh.xy, max(1e-6, nh.z)))
+
 
