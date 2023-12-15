8b9d6e9b02d6abf638e4a507698bc1251262aa80
bilibili <zhou.zq@uci.edu>
2023-12-14 18:37:52 -0800

    direct: turn off MIS by default

--- direct.py
+++ direct.py
@@ -7,6 +7,11 @@ from .interaction import read_bsdf, write_bsdf_grad, surface_interact
 from .light import sample_light
 from .onb import *
 
+# MIS off: Only draw light samples. Good for small lights.
+# MIS on: Draw light & bsdf samples, at cost of ~2.6x computation.
+#         Useful for large lights / glossy surfaces.
+use_MIS = False
+
 @luisa.func
 def direct_estimator(ray, sampler, heap, accel, light_count, material_buffer, texture_res):
     hit = accel.trace_closest(ray, -1)
@@ -20,7 +25,7 @@ def direct_estimator(ray, sampler, heap, accel, light_count, material_buffer, te
     if any(emission > float3(0.0)):
         return emission
 
-
+    # fetch texture
     mat = read_bsdf(it.uv, material_buffer, texture_res)
     diffuse = mat.xyz
     roughness = mat.w
@@ -34,32 +39,38 @@ def direct_estimator(ray, sampler, heap, accel, light_count, material_buffer, te
     onb = make_onb(it.ns)
     wo_local = onb.to_local(-ray.get_dir())
     wi_light_local = onb.to_local(light.wi)
-    # there could still be light from back face due to imperfect occlusion
+    # Discard light from back face due to imperfect occlusion / non-manifolds
     if not occluded and wi_light_local.z > 0.0:
         bsdf = ggx_brdf(wo_local, wi_light_local, diffuse, specular, roughness)
         # mis_weight = balanced_heuristic(light.pdf, pdf_bsdf)
-        mis_weight = 0.0
+        if use_MIS:
+            # TODO MIS
+            mis_weight = 0.0
+        else:
+            mis_weight = 1.0
         radiance += bsdf * mis_weight * light.eval / max(light.pdf, 1e-4)
 
-    # pdf sample (next bounce)
-    wi_local = ggx_sample(wo_local, diffuse, specular, roughness, sampler)
-    pdf_bsdf = ggx_sample_pdf(wo_local, wi_local, diffuse, specular, roughness)
-    wi = onb.to_world(wi_local)
-    if dot(wi, it.ng) < 1e-4 or wi_local.z < 1e-4:
-        return radiance
-    ray = luisa.make_ray(luisa.offset_ray_origin(it.p, it.ng), wi, 0.0, 1e30)
-    beta = ggx_brdf(wo_local, wi_local, diffuse, specular, roughness) / pdf_bsdf
-    # JUST REPEAT..
-    hit = accel.trace_closest(ray, -1)
-    if hit.miss():
-        return radiance
-    it = surface_interact(hit, heap)
-    # backfacing geometry
-    if dot(-ray.get_dir(), it.ng) < 1e-4 or dot(-ray.get_dir(), it.ns) < 1e-4:
-        return radiance
-    emission = heap.buffer_read(float3, 23333, hit.inst)
-    if any(emission > float3(0.0)):
-        return radiance + beta * emission
+    if use_MIS:
+        # pdf sample (next bounce)
+        wi_local = ggx_sample(wo_local, diffuse, specular, roughness, sampler)
+        pdf_bsdf = ggx_sample_pdf(wo_local, wi_local, diffuse, specular, roughness)
+        wi = onb.to_world(wi_local)
+        if dot(wi, it.ng) < 1e-4 or wi_local.z < 1e-4:
+            return radiance
+        ray = luisa.make_ray(luisa.offset_ray_origin(it.p, it.ng), wi, 0.0, 1e30)
+        beta = ggx_brdf(wo_local, wi_local, diffuse, specular, roughness) / pdf_bsdf
+        # TODO MIS
+        # Just repeat tracing...
+        hit = accel.trace_closest(ray, -1)
+        if hit.miss():
+            return radiance
+        it = surface_interact(hit, heap)
+        # backfacing geometry
+        if dot(-ray.get_dir(), it.ng) < 1e-4 or dot(-ray.get_dir(), it.ns) < 1e-4:
+            return radiance
+        emission = heap.buffer_read(float3, 23333, hit.inst)
+        if any(emission > float3(0.0)):
+            return radiance + beta * emission
 
     return radiance
 
@@ -87,7 +98,6 @@ def direct_estimator_backward(ray, sampler, heap, accel, light_count,
             roughness = mat.w
             specular = 0.04
             bsdf = ggx_brdf(onb.to_local(-ray.get_dir()), onb.to_local(light.wi), diffuse, specular, roughness)
-            # mis_weight = balanced_heuristic(light.pdf, pdf_bsdf)
             mis_weight = 1.0
             le = bsdf * cos_wi_light * mis_weight * light.eval / max(light.pdf, 1e-4)
             backward(le, le_grad)
